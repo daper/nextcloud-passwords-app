6c5bf54a21387724c29b228b98b131796d19370d
David Peralta <david@daper.email>
2020-08-06 20:19:03 +0200

    Bump react-native version to 0.63.2

--- tools/fix-packages.sh
+++ tools/fix-packages.sh
@@ -2,9 +2,9 @@
 
 BASE_DIR=$(git rev-parse --show-toplevel)
 sed -Ei 's/ compile / implementation /g' $BASE_DIR/node_modules/react-native-sqlcipher-2/android/build.gradle
-sed -Ei 's/compileSdkVersion [0-9]{2}/compileSdkVersion 28/g' $BASE_DIR/node_modules/react-native-sqlcipher-2/android/build.gradle
+sed -Ei 's/compileSdkVersion [0-9]{2}/compileSdkVersion 29/g' $BASE_DIR/node_modules/react-native-sqlcipher-2/android/build.gradle
 sed -Ei 's/ compile / implementation /g' $BASE_DIR/node_modules/react-native-fingerprint-scanner/android/build.gradle
 sed -Ei '/buildToolsVersion/d' $BASE_DIR/node_modules/react-native-fingerprint-scanner/android/build.gradle
-sed -Ei 's/compileSdkVersion [0-9]{2}/compileSdkVersion 28/g' $BASE_DIR/node_modules/react-native-fingerprint-scanner/android/build.gradle
-sed -Ei 's/targetSdkVersion [0-9]{2}/targetSdkVersion 28/g' $BASE_DIR/node_modules/react-native-fingerprint-scanner/android/build.gradle
-sed -Ei 's/1\.2\.71/1\.3\.0/g' $BASE_DIR/node_modules/react-native-webview/android/build.gradle
+sed -Ei 's/compileSdkVersion [0-9]{2}/compileSdkVersion 29/g' $BASE_DIR/node_modules/react-native-fingerprint-scanner/android/build.gradle
+sed -Ei 's/targetSdkVersion [0-9]{2}/targetSdkVersion 29/g' $BASE_DIR/node_modules/react-native-fingerprint-scanner/android/build.gradle
+# sed -Ei 's/1\.2\.71/1\.3\.0/g' $BASE_DIR/node_modules/react-native-webview/android/build.gradle
